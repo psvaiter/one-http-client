ffa83dee8440d5cd20a45e4899dc71619119dce6
Patrick Svaiter <psvaiter@stone.com.br>
2018-06-26 17:16:54 -0300

    Renames test methods to match name of Send().

--- test/OneHttpClient.UnitTests/HttpServiceTests.cs
+++ test/OneHttpClient.UnitTests/HttpServiceTests.cs
@@ -21,7 +21,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "GET string should work.")]
         [Trait("Category", "GET")]
-        public void SendJson_to_GET_string_should_return_200()
+        public void Send_GET_string_should_return_200()
         {
             var expectedStatusCode = 200;
             var expectedContentType = "text/plain";
@@ -39,7 +39,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "GET JSON should work.")]
         [Trait("Category", "GET")]
-        public void SendJson_to_GET_json_should_return_200()
+        public void Send_GET_json_should_return_200()
         {
             var expectedStatusCode = 200;
             var expectedContentType = "application/json";
@@ -57,7 +57,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "GET should receive status code 500 without body when that was returned by server.")]
         [Trait("Category", "GET")]
-        public void SendJson_to_GET_internal_error_should_return_500()
+        public void Send_GET_internal_error_should_return_500()
         {
             var expectedStatusCode = 500;
             var expectedResponseBody = string.Empty;
@@ -74,7 +74,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "GET JSON should have deserialized response successfully when requested.")]
         [Trait("Category", "GET")]
-        public void SendJson_to_GET_json_deserialized_should_return_200()
+        public void Send_GET_json_deserialized_should_return_200()
         {
             var expectedStatusCode = 200;
             var expectedContentType = "application/json";
@@ -96,7 +96,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "GET should return the response without data deserialized when JSON is invalid.")]
         [Trait("Category", "GET")]
-        public void SendJson_to_GET_invalid_json_deserialized_should_return_200_without_data_deserialized()
+        public void Send_GET_invalid_json_deserialized_should_return_200_without_data_deserialized()
         {
             // Test: Get a response asking for deserialization. The invalid data format in response will make deserialization fail.
             // Result: Everything OK. There will be no data deserialized.
@@ -119,7 +119,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "GET should return the response without data deserialized when Content-Type is not supported for deserialization by HttpService.")]
         [Trait("Category", "GET")]
-        public void SendJson_to_GET_data_deserialized_with_unsupported_content_type_deserialization_should_return_200_without_data_deserialized()
+        public void Send_GET_data_deserialized_with_unsupported_content_type_deserialization_should_return_200_without_data_deserialized()
         {
             // Test: Get a response asking for deserialization with content-type not supported for deserialization.
             // Result: Everything OK. There will be no data deserialized.
@@ -142,7 +142,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "POST should work. Return the same data sent with status code 200 when /echo is requested.")]
         [Trait("Category", "POST")]
-        public void SendJson_to_POST_data_should_return_200_with_data_sent()
+        public void Send_POST_data_should_return_200_with_data_sent()
         {
             var expectedStatusCode = 200;
             var expectedContentType = "application/json";
@@ -160,7 +160,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "PUT should work. Return the same data sent with status code 200 when /echo is requested.")]
         [Trait("Category", "PUT")]
-        public void SendJson_to_PUT_data_should_return_200_with_data_sent()
+        public void Send_PUT_data_should_return_200_with_data_sent()
         {
             var expectedStatusCode = 200;
             var expectedContentType = "application/json";
@@ -178,7 +178,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "PACTH should work. Return the same data sent with status code 200 when /echo is requested.")]
         [Trait("Category", "PATCH")]
-        public void SendJson_to_PATCH_data_should_return_200_with_data_sent()
+        public void Send_PATCH_data_should_return_200_with_data_sent()
         {
             var expectedStatusCode = 200;
             var expectedContentType = "application/json";
@@ -196,7 +196,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "DELETE should work.")]
         [Trait("Category", "DELETE")]
-        public void SendJson_to_DELETE_data_should_return_200()
+        public void Send_DELETE_data_should_return_200()
         {
             var expectedStatusCode = 200;
             var expectedResponseBody = string.Empty;
@@ -212,7 +212,7 @@ namespace OneHttpClient.UnitTests
 
         [Fact(DisplayName = "Serialization/Deserialization in snake_case should work when this naming strategy is chosen.")]
         [Trait("Category", "POST")]
-        public void SendJson_in_snake_case_should_be_serialized_and_deserialized_correctly()
+        public void Send_in_snake_case_should_be_serialized_and_deserialized_correctly()
         {
             var expectedStatusCode = 200;
             var expectedResponseBody = HttpServerFixture.SampleTextJsonSnakeCase;
