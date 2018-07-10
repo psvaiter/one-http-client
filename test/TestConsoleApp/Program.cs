c302bcd253303c0ba216f457f49eb2bb40c15cc8
Patrick Svaiter <psvaiter@stone.com.br>
2018-07-10 18:29:23 -0300

    Adds a sample console app under tests folder.

---
+++ test/TestConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // NLog: setup the logger first to catch all errors
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("Building IWebHost...");
+                BuildWebHost().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        private static IWebHost BuildWebHost()
+        {
+            var webHost = WebHost.CreateDefaultBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                    //logging.AddFilter("OneHttpClient.HttpService", LogLevel.None);
+                })
+                .UseNLog()
+                .UseStartup<Startup>()
+                .Build();
+
+            return webHost;
+        }
+    }
+}
